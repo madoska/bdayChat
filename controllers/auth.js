7fc07c532542d777402bdec49a50007edf4aa1f6
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-08-30 15:13:05 +0200

    created login function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,38 @@
 const Users = require('../models/User');
 const passport = require('../passport/passport');
 
-const signup = async (req,res,next) => {
+const signup = async (req, res, next) => {
     const username = req.body.username;
     let password = req.body.password;
-    
-    const user = new Users({username : 'username'});
+
+    const user = new Users({ username: 'username' });
     await user.setPassword('password');
     await user.save().then(result => {
         res.json({
-            'status' : 'success'
+            'status': 'success'
         })
     }).catch(error => {
         res.json({
-           'status': 'error' 
+            'status': 'error'
         })
     });
 }
 
+const login = async (req, res, next) => {
+    const { user } = await User.authenticate()(req.body.username, req.body.password).then(result => {
+        res.json({
+            'status': 'success',
+            'data': {
+                'user': result
+            }
+        })
+    }).catch(error => {
+        res.json({
+            'status': 'error',
+            'message': error
+        })
+    })
+}
+
 module.exports.signup = signup;
+module.exports.login = login;
