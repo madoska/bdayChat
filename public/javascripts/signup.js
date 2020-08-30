004c9b61cbfc68a2e54d888af3aee0170dfe7ded
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-08-30 18:40:53 +0200

    added fetch function for signup

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,3 +1,23 @@
-document.querySelector(".btn").addEventListener('click', function(){
-    console.log('hi')
+const btnSignup = document.querySelector('.btn').addEventListener('click', function () {
+    let username = document.querySelector('#username').value;
+    let password = document.querySelector('#password').value;
+
+    fetch('http://localhost:3000/users/signup', {
+        method: "post",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'username': username,
+            'password': password
+        })
+    }).then(response => {
+        return response.json();
+    }).then(json => {
+        if(json.status === 'success'){
+            let feedback = document.querySelector('.alert');
+            feedback.textContent = "Sign up successful!";
+            feedback.classList.remove('hidden');
+        }
+    })
 })
