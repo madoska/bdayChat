e88ac959db9dbb700ab11639772ee3feb29ce667
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-08-31 21:09:36 +0200

    created token fetch from header for chat

---
+++ public/javascripts/app.js
@@ -0,0 +1,13 @@
+fetch('http://localhost:3000/views/chat', {
+    'headers': {
+        'Authorization': 'Bearer' + localStorage.getItem('token')
+    }
+}).then(result => {
+    return result.json();
+}).then(json => {
+    console.log(json)
+}).catch(err => {
+    console.log("Error")
+})
+
+console.log("hello")
