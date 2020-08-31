6b0ecbcf9a672921acd1de27c2b2101eb02bd2ed
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-08-31 16:09:40 +0200

    created and fixed signup and login controller functions

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,40 +1,62 @@
 const Users = require('../models/User');
-const passport = require('../passport/passport');
 
 const signup = async (req, res, next) => {
+    console.log(req.body);
     const username = req.body.username;
     const password = req.body.password;
     const bday = req.body.bday;
 
-    const user = new Users({ username: username });
+    const user = new Users({
+        username: username
+    });
     user.bday = bday;
     await user.setPassword(password);
-    await user.save().then(result => {
-        res.json({
-            'status': 'success'
-        })
-    }).catch(error => {
-        res.json({
-            'status': 'error'
-        })
-    });
+    await user.save()
+        .then(result => {
+            console.log(result.id);
+            console.log(result)
+
+            let token = jwt.sign({
+                id: result._id
+            }, "{this is a secret}");
+
+            console.log(token);
+
+            res.json({
+                'status': 'success',
+                'data': {
+                    'token': token
+                }
+            })
+        }).catch(error => {
+            res.json({
+                'status': 'error'
+            })
+        });
 }
 
 const login = async (req, res, next) => {
-    const { user } = await Users.authenticate()(req.body.username, req.body.password).then(result => {
-        res.json({
-            'status': 'success',
-            'data': {
-                'user': result
+    const user = await Users.authenticate()(req.body.username, req.body.password).then(result => {
+        if (!result.user) {
+            console.log("wrong login")
+        } else {
+            console.log("okay")
+        }
+
+        return res.json({
+            "status": "success",
+            "data": {
+                "user": result
             }
-        })
+        });
     }).catch(error => {
         res.json({
-            'status': 'error',
-            'message': error
+            "status": "error",
+            "message": error
         })
-    })
-}
+    });
+};
+
 
 module.exports.signup = signup;
 module.exports.login = login;
