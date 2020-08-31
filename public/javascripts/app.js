89c3329acf93f897543230a6b9c87717a6612372
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-09-01 01:33:30 +0200

    changed css styling back to basics

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -10,4 +10,26 @@ fetch('http://localhost:3000/views/chat', {
     console.log("Error")
 })
 
-console.log("hello")
+let input = document.querySelector('#msg');
+input.addEventListener('keyup', e => {
+    if (e.keyCode === 13) {
+        let text = input.value;
+        fetch('http://localhost:3000/views/chat', {
+            method: 'post',
+            'headers': {
+                'Content-type': 'application/json',
+                'Authorization': 'Bearer' + localStorage.getItem('token')
+            },
+            body: JSON.stringify({
+                'text': text
+            })
+        }).then(result => {
+            return result.json();
+        }).then(json => {
+            console.log(json)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+    e.preventDefault();
+})
