dd2d40a164a77c13169abe9d898700c4f23f9193
Medina Dadurgova <medina-dadurgova@hotmail.com>
2020-08-31 21:06:08 +0200

    fixed chat router + jwt authentication in comment

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/views/chat', passport.authenticate('jwt', { session: false }), chatRouter);
+app.use('/views', /*passport.authenticate('jwt', { session: false }),*/ chatRouter);
 
 // catch 404 and forward to errondler
 app.use(function (req, res, next) {
